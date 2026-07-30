da9a5d0c0a06ff0a52f9e4073c34a1cc75d948b0
Catevika <dominique.bello@outlook.fr>
2026-07-30 19:22:30 +0200

    Replace __dirname with import.meta.dirname

--- vite.config.ts
+++ vite.config.ts
@@ -67,7 +67,7 @@ export default defineConfig(({ mode }) => {
 
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "./src"),
+        "@": path.resolve(import.meta.dirname, "./src"),
       },
     },
   };
