5e2c8877abf773c89118f38009b6148300193790
Catevika <dominique.bello@outlook.fr>
2026-08-01 13:05:40 +0200

    Fix playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   preserveOutput: "failures-only",
 
   use: {
-    baseURL: "http://localhost:3000",
+    baseURL: "http://127.0.0.1:3000",
     // video: "on", // record videos for ALL tests
     trace: "on", // record trace for ALL tests
     screenshot: "only-on-failure",
@@ -29,7 +29,7 @@ export default defineConfig({
 
   webServer: {
     command: "npm run preview",
-    url: "http://localhost:3000",
+    url: "http://127.0.0.1:3000",
     reuseExistingServer: !process.env.CI,
     timeout: 120000,
   },
